3671192b5c7fa43bec1a32299f7556b69cb1295e
Andy Gayton <andy@thecablelounge.com>
2018-04-26 12:09:29 -0700

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,26 +2,34 @@
 module.exports = {
   plugins: [
 
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `images`,
-				path: `${__dirname}/src/images/`
-			}
-		},
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-98252461-1",
+        head: false,
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`
+      }
+    },
 
     `gatsby-plugin-sharp`, 
     `gatsby-transformer-sharp`,
 
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `markdown`,
-				path: `${__dirname}/src/markdown/`
-			}
-		},
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown`,
+        path: `${__dirname}/src/markdown/`
+      }
+    },
 
-		`gatsby-transformer-remark`,
+    `gatsby-transformer-remark`,
 
     {
       resolve: `gatsby-plugin-typography`,

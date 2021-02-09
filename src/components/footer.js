281293ee09f5e8aa4866a3ef039db1c8643ba179
Andy Gayton <andy@thecablelounge.com>
2021-02-09 10:04:14 -0800

    add link to Canadian website

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,16 @@
 export default () =>
   <div style={{ padding: '10px', borderTop: '1px #aaa solid', textAlign: 'center', fontSize: '60%' }}>
     <br/>
-    Oh Hai
     1300 Clay Street, Suite 600 ~ Oakland, CA 94612 ~ (415) 230-2860
     ~ © Kosinski and Thiagaraj, LLP 2020
+    <br/>
+    <br/>
+    <a href="https://ktlawsf.ca">View our Canadian website</a>
     <br/>
     <br/>
     This website contains general information that is intended, but not
     guaranteed, to be correct.
     <br/>
         This website is not intended to be a source of legal advice and visiting this website does not create an attorney-client relationship.
+    <br/>
   </div>

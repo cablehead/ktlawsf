c98d5492f515d7e019da81590681dd051bc316bc
Andy Gayton <andy@thecablelounge.com>
2022-08-27 12:41:15 -0400

    add link on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,6 +63,9 @@ export default (props) =>
       <b>Kosinski and Thiagaraj, LLP</b> is your advocate for fair and lawful treatment at work. <br/><b>We are on your side.</b>
     </h1>
     <div>
+      <p style={{ textAlign: 'center' }}>
+      <a href="/virgin-america-class-action">Virgin America Flight Attendants, Click Here for More Info</a>
+      </p>
       <p>
       Finding an advocate who will
       listen closely to your case and provide you with individual service is
